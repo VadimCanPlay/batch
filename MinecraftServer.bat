0dd21046d8d52725ad4354c5d82e00813091ef16
VadimCanPlayOfficial <141765567+VadimCanPlay@users.noreply.github.com>
2024-09-13 19:52:42 +0300

    Update MinecraftServer.bat

--- MinecraftServer.bat
+++ MinecraftServer.bat
@@ -13,17 +13,17 @@ echo [] > whitelist.json
 echo none > server.properties
 echo eula=true > eula.txt
 echo > server.jar
-echo [main/INFO] Starting minecraft server version 1.21
-echo [Server thread/INFO] Loading prpperties
-echo [Server thread/INFO] Deafult game type: SURVIVAL
-echo [Server thread/INFO] Generating keypair
-echo [Server thread/INFO] Starting Minecraft server on *:25565
-echo [Server thread/INFO] Using deafult channel type
-echo [Server thread/INFO] Preparing level "world"
-echo [Server thread/INFO] Preparing start region for dimension minecraft:overworld
-echo [Worker-Main-14/INFO] Preparing spawn area: 0%
-echo [Worker-Main-14/INFO] Preparing spawn area: 100%
-echo [Server thread/INDO] Done! (0s) For help, type "help"
+echo [%time%] [main/INFO] Starting minecraft server version 1.21
+echo [%time%] [Server thread/INFO] Loading prpperties
+echo [%time%] [Server thread/INFO] Deafult game type: SURVIVAL
+echo [%time%] [Server thread/INFO] Generating keypair
+echo [%time%] [Server thread/INFO] Starting Minecraft server on *:25565
+echo [%time%] [Server thread/INFO] Using deafult channel type
+echo [%time%] [Server thread/INFO] Preparing level "world"
+echo [%time%] [Server thread/INFO] Preparing start region for dimension minecraft:overworld
+echo [%time%] [Worker-Main-14/INFO] Preparing spawn area: 0%
+echo [%time%] [Worker-Main-14/INFO] Preparing spawn area: 100%
+echo [%time%] [Server thread/INDO] Done! (0s) For help, type "help"
 set /p option=
 if %option% == stop goto stop
 if /i "%command:~0,2%"=="op" goto op

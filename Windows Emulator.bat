2da3a2cba7ec4921e33279c26456adfc016f8715
VadimCanPlayOfficial <141765567+VadimCanPlay@users.noreply.github.com>
2024-09-14 19:37:59 +0300

    Add files via upload

---
+++ Windows Emulator.bat
@@ -0,0 +1,40 @@
+@echo off
+chcp 65001 >nul 2>&1
+title Windows Emulator
+:main
+echo Launching...
+ping localhost -n 5 >nul
+goto PC
+:PC
+cls
+echo.
+echo  🖥️
+echo This PC
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo ------------------------------------------------------------------------------------------------------------------------
+echo                                                   🪟 🔍Search 📁 🗒️
+pause >nul
+goto PC

